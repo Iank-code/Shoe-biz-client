3da2c973a5ab8faed3b8d2d4c98330dac009a2c2
Ian Kamau <kian99564@gmail.com>
2024-01-02 11:57:47 +0300

    Working on landing-top-part

--- src/components/landing-top/LandingTop.tsx
+++ src/components/landing-top/LandingTop.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import "./landingTop.css"
+
 import landingImg from "./../../../public/landing/airforce1.png";
 import landingUp from "./../../../public/landing/airforce1-up.png";
 import landingDown from "./../../../public/landing/airforce1-down.png";
-import Image from "next/image";
+
+import fashionImg from "./../../../public/landing/marketing/fashion.jpg";
+import fitnesstImg from "./../../../public/landing/marketing/fitness.jpg";
+import officialImg from "./../../../public/landing/marketing/official.jpg";
+
+const marketImgs = [
+  { img: fitnesstImg, name: "FITNESS" },
+  { img: fashionImg, name: "FASHION" },
+  { img: officialImg, name: "OFFICIAL" },
+];
 
 export default function LandingTop() {
   return (
@@ -19,7 +31,35 @@ export default function LandingTop() {
           Nike, Puma, and More.
         </p>
       </div>
-      <div></div>
+
+      <div className="flex items-center px-[2em] mobile:px-[1em] mt-[1em] justify-center gap-[3em] mobile:flex-col">
+        {marketImgs.map((image, index) => {
+          
+          return (
+            <div key={index}>
+              <Image
+                src={image.img}
+                alt={image.name}
+                className="rounded-[2em] h-[500px] w-[500px] mobile:h-[350px] mobile:w-[350px]"
+              />
+              <span
+                style={{
+                  position: "relative",
+                  bottom: "4em",
+                  left: "7em",
+                  color: "white",
+                  letterSpacing: "11px",
+                  fontSize: "20px",
+                  fontWeight: 800,
+                }}
+                className="top-landing"
+              >
+                {image.name}
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }

e160628965f2728045b284552a7f07f6a43104d4
Ian Kamau <kian99564@gmail.com>
2024-01-03 23:07:57 +0300

    Making navbar responsive

--- src/components/navbar/sidebar/Sidebar.tsx
+++ src/components/navbar/sidebar/Sidebar.tsx
@@ -1,5 +1,29 @@
+import { nav_top_url } from "@/utils/helpers/url.helper";
+import { IconX } from "@tabler/icons-react";
+import Link from "next/link";
 import React from "react";
 
-export default function Sidebar() {
-  return <div className="h-[100px] b-white text-black z-[999] absolute">Sidebar</div>;
+export default function Sidebar({ setOpenSideBar }: any) {
+  return (
+    <div className="h-[100vh] w-[250px] pt-[2.3em] px-[1.5em] bg-white text-black z-[999] absolute">
+      <div className="flex justify-between mb-[2em]">
+        <span>Shoe biz </span>
+        <IconX
+          className="hidden mobile:flex "
+          onClick={() => {
+            setOpenSideBar(false);
+          }}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        {nav_top_url.map((link, index) => {
+          return (
+            <Link href={link} key={index}>
+              {link}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }

8a44311c6faee00227d4a36006efada6dd09367f
Ian Kamau <kian99564@gmail.com>
2024-01-03 16:44:02 +0300

    Setting up toolking

---
+++ src/lib/store/slices/workflows/login.slice.ts
@@ -0,0 +1,38 @@
+import { loginItem } from "@/lib/typings/core/login.typings";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./../../index";
+
+const initialState: loginItem = {
+  id: "",
+  imageUrl: "",
+  accessToken: "",
+};
+
+// export const loginSlice = createSlice({
+//   name: "login_slice",
+//   initialState,
+//   reducers: {
+//     updateLoginState: (state, action) => {
+//       state.id = action.payload.id;
+//       state.imageUrl = action.payload.imageUrl;
+//       state.accessToken = action.payload.accessToken;
+//     },
+//   },
+// });
+
+export const loginSlice = createSlice({
+  name: "login_slice",
+  initialState,
+  reducers: {
+    updateLoginState: (state, action: PayloadAction<loginItem>) => {
+      const { id, imageUrl, accessToken } = action.payload;
+      state.id = id;
+      state.imageUrl = imageUrl;
+      state.accessToken = accessToken;
+    },
+  },
+});
+
+export const { updateLoginState } = loginSlice.actions;
+export const addLoginState = (state: RootState) => state.login;
+export default loginSlice.reducer;

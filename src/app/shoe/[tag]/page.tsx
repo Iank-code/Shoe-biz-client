f2a645291fb8175dffba98a8478a29482ef12611
Ian Kamau <kian99564@gmail.com>
2024-01-14 17:00:56 +0300

    User can get products using params

--- src/app/shoe/[tag]/page.tsx
+++ src/app/shoe/[tag]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
@@ -6,8 +6,6 @@ import { useEffect } from "react";
 
 const ProductPage = ({ params }: any) => {
   useEffect(() => {
-    // You can send the tag to your backend here
-    // Replace this with your actual API call to send data to the backend
     fetch(`http://localhost:5000/api/v1/product/shop?tag=${params.tag}`)
       .then((response) => response.json())
       .then((data) => {
@@ -17,7 +15,7 @@ const ProductPage = ({ params }: any) => {
         console.error("Error sending tag to backend:", error);
       });
   }, [params.tag]);
- 
+
   // Your component rendering logic goes here
 
   return (

fd40593e1ace260ccd5753f15b87aadbe711cd54
Iank-code <kian99564@gmail.com>
2024-11-01 00:37:37 +0300

    feat: added service object class

--- src/services/product_service.ts
+++ src/services/product_service.ts
@@ -1,3 +1,7 @@
 class productService {
+    /**
+     * Complete addToWishList function that adds product to wishlist for authenticated users
+     * Place all service/api requests for products here
+     */
   async addToWishList() {}
 }
